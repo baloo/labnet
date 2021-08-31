02e6d228a032706d4dcf3ef49000a98e916474ec
Arthur Gautier <baloo@superbaloo.net>
2021-08-31 22:22:33 +0000

    use unshare and dnsmasq
    Signed-off-by: Arthur Gautier <baloo@superbaloo.net>

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i "make -f" -p gnumake qemu clang-tools
-
+#!nix-shell -i "make -f" -p gnumake qemu "dnsmasq.overrideAttrs(old: {preBuild = ''makeFlagsArray=(\"COPTS=-DNO_INOTIFY\")'';})"
+ 
 labnet: labnet.o
 
 .PHONY: test
 test: labnet
-	./$< ./start-qemu.sh
+	unshare -f --pid -r -n -m -U --map-group=0 -S 0 --mount-proc ./start-qemu.sh http://192.168.0.1:3030/foo
 
 .PHONY: format
 format:
--- start-qemu.sh
+++ start-qemu.sh
@@ -4,7 +4,16 @@ set -x
 
 ip link set lo up
 ip link add br0 type bridge
+ip link set br0 up
 ip addr add 192.168.0.1/24 dev br0
+DHCPD_PID=$(mktemp)
+id
+echo 8192 > /proc/sys/fs/inotify/max_user_instances
+echo 524288 > /proc/sys/fs/inotify/max_user_watches
+dnsmasq --leasefile-ro --dhcp-broadcast -p 0 -F 192.168.0.2,192.168.0.10,255.255.255.0,1h -M $1 --pid-file=$DHCPD_PID --listen-address=192.168.0.1 -d 2>&1 >/dev/null &
 
-qemu-kvm -m 1G -cpu host -serial mon:stdio -net nic,model=virtio,id=net0 -net bridge,id=net0,br=br0 -nographic
-
+qemu-kvm -m 1G -cpu host -serial mon:stdio \
+ -netdev tap,id=nd1,script="./helper.sh" \
+ -device virtio-net-pci,netdev=nd1 \
+ -nographic
+kill $(cat $DHCPD_PID)
